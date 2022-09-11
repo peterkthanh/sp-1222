85919f96c1afed8cf6f8986dcc70121ea57b734d
PeterThanh <peterkthanh@gmaik.com>
2022-09-12 03:28:32 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom"
+import { AnimatePresence } from "framer-motion";
+import { Headers, MainContainer, CreateContainer } from "./components"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence>
+      <div className=" w-screen h-auto flex flex-col bg-primary">
+        <Headers />
+        <main className="mt-24 p-8 w-full">
+          <Routes>
+            <Route path="/" element={<MainContainer />} />
+            <Route path="/CreatItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div >
+    </AnimatePresence>
   );
 }
 
---
+++ src/firebase.config.js
@@ -0,0 +1,22 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZx6awBfvTU5FmuqB1ya_tMl7AQX_9-aI",
+    authDomain: "sp-1-b2e7c.firebaseapp.com",
+    databaseURL: "https://sp-1-b2e7c-default-rtdb.firebaseio.com",
+    projectId: "sp-1-b2e7c",
+    storageBucket: "sp-1-b2e7c.appspot.com",
+    messagingSenderId: "529531004149",
+    appId: "1:529531004149:web:141fcf3a881ef9e43e51bc",
+};
+
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+const firestore = getFirestore(app);
+
+export { app, db, storage, firestore };
